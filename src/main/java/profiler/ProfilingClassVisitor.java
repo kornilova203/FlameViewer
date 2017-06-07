438f4e30c47f018a921b806ad3faf361b517695c
kornilova-l <kornilova203@gmail.com>
2017-06-07 11:34:41 +0300

    Move agent classes to separate files

---
+++ src/main/java/profiler/ProfilingClassVisitor.java
@@ -0,0 +1,20 @@
+package profiler;
+
+import org.objectweb.asm.ClassVisitor;
+import org.objectweb.asm.MethodVisitor;
+import org.objectweb.asm.Opcodes;
+
+class ProfilingClassVisitor extends ClassVisitor {
+    ProfilingClassVisitor(ClassVisitor cv) {
+        super(Opcodes.ASM5, cv);
+    }
+
+    @Override
+    public MethodVisitor visitMethod(int access, String name, String desc, String signature, String[] exceptions) {
+        MethodVisitor mv = cv.visitMethod(access, name, desc, signature, exceptions);
+        if (mv != null && !name.equals("toString") && !name.equals("main")) {
+            mv = new ProfilingMethodVisitor(access, name, desc, mv);
+        }
+        return mv;
+    }
+}
