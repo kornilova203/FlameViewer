0c777e3f07e01a558fd9d75fd487c2d5d51d0387
kornilova-l <kornilova203@gmail.com>
2017-06-16 20:54:11 +0300

    Added drawTree function

--- visualization/js/src/main.js
+++ visualization/js/src/main.js
@@ -5,10 +5,36 @@ function createSectionForThread(threadId) {
     $("main").append(templates.tree.getSectionForThread({threadId: threadId}).content);
 }
 
+
+function drawTree(tree) {
+    const stage = new createjs.Stage("canvas-" + tree.getThreadid());
+    const graphics = new createjs.Graphics();
+    graphics.beginFill("blue").drawRect(20, 20, 100, 50);
+
+    const shape = new createjs.Shape(graphics);
+    stage.addChild(shape);
+    stage.update();
+
+}
 /**
  * Main function
  */
 $(window).on("load", function () {
+    // const stage = new createjs.Stage("canvas");
+    // const text = new createjs.Text("Hello World", "20px Arial", "#ff7700");
+    // // text.x = 100;
+    // // text.textBaseline = "alphabetic";
+    // stage.addChild(text);
+    // stage.update();
+    //
+    // const circle = new createjs.Shape();
+    // circle.graphics.beginFill("DeepSkyBlue").drawCircle(0, 0, 50);
+    // circle.x = 0;
+    // circle.y = 0;
+    // stage.addChild(circle);
+    // stage.update();
+
+
     const request = new XMLHttpRequest();
     request.open("GET", "http://localhost:63343/flamegraph-profiler/trees/original-tree", true);
     request.responseType = "arraybuffer";
@@ -18,6 +44,7 @@ $(window).on("load", function () {
         const byteArray = new Uint8Array(arrayBuffer);
         const tree = TreeProto.Tree.deserializeBinary(byteArray);
         createSectionForThread(tree.getThreadid());
+        drawTree(tree);
     };
     request.send();
 });
