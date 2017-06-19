955e4fa6d0d44c60f1f627cbd44a5c596d08189b
kornilova-l <kornilova203@gmail.com>
2017-06-19 15:20:05 +0300

    Removed unused id from soy template

--- visualization/js/src/tree-templates.js
+++ visualization/js/src/tree-templates.js
@@ -11,7 +11,7 @@ if (typeof templates.tree == 'undefined') { templates.tree = {}; }
 
 
 templates.tree.getSectionForThread = function(opt_data, opt_ignored) {
-  return soydata.VERY_UNSAFE.ordainSanitizedHtml('<section id="section-' + soy.$$escapeHtmlAttribute(opt_data.threadId) + '"><h2>' + soy.$$escapeHtml(opt_data.threadId) + '</h2><canvas id="canvas-' + soy.$$escapeHtmlAttribute(opt_data.threadId) + '" width="700" height="' + soy.$$escapeHtmlAttribute(opt_data.canvasHeight) + '"></canvas></section>');
+  return soydata.VERY_UNSAFE.ordainSanitizedHtml('<section><h2>' + soy.$$escapeHtml(opt_data.threadId) + '</h2><canvas id="canvas-' + soy.$$escapeHtmlAttribute(opt_data.threadId) + '" width="700" height="' + soy.$$escapeHtmlAttribute(opt_data.canvasHeight) + '"></canvas></section>');
 };
 if (goog.DEBUG) {
   templates.tree.getSectionForThread.soyTemplateName = 'templates.tree.getSectionForThread';
