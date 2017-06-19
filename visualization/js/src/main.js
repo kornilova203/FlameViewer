5a2e75b58ef34c5d22304058df2c4dbbed717012
kornilova-l <kornilova203@gmail.com>
2017-06-19 17:18:56 +0300

    Implemented multiple choice form

--- visualization/js/src/main.js
+++ visualization/js/src/main.js
@@ -5,6 +5,25 @@ const TreeProto = require('./tree_pb');
 
 const trees = [];
 
+function drawTrees(trees) {
+    if (trees.length === 3) {
+        let minStartTime = trees[0].getTreeInfo().getStartTime();
+        let maxFinishTime = trees[0].getTreeInfo().getStartTime() + trees[0].getWidth();
+        for (let i = 1; i < trees.length; i++) {
+            const startTime = trees[i].getTreeInfo().getStartTime();
+            if (startTime < minStartTime) {
+                minStartTime = startTime;
+            }
+            if (startTime + trees[i].getWidth() > maxFinishTime) {
+                maxFinishTime = startTime + trees[i].getWidth();
+            }
+        }
+        for (let i = 0; i < trees.length; i++) {
+            new Drawer(trees[i], minStartTime, maxFinishTime);
+        }
+    }
+}
+
 $(window).on("load", function () {
     // const request = new XMLHttpRequest();
     // request.open("GET", "http://localhost:63343/flamegraph-profiler/trees/original-tree", true);
@@ -21,39 +40,21 @@ $(window).on("load", function () {
 
     // INPUT form for easier debugging
     $('#file').on('change', function (e) {
-        const file = e.target.files[0]; // FileList object
-        const reader = new FileReader();
-
-        reader.onload = (function (theFile) {
-            reader.readAsArrayBuffer(theFile);
-            reader.addEventListener("load", function () {
-                const arrayBuffer = reader.result;
-                const byteArray = new Uint8Array(arrayBuffer);
-                trees.push(TreeProto.Tree.deserializeBinary(byteArray));
-                if (trees.length === 3) {
-                    let minStartTime = trees[0].getTreeInfo().getStartTime();
-                    let maxFinishTime = trees[0].getTreeInfo().getStartTime() + trees[0].getWidth();
-                    for (let i = 1; i < trees.length; i++) {
-                        const startTime = trees[i].getTreeInfo().getStartTime();
-                        if (startTime < minStartTime) {
-                            minStartTime = startTime;
-                        }
-                        if (startTime + trees[i].getWidth() > maxFinishTime) {
-                            maxFinishTime = startTime + trees[i].getWidth();
-                        }
+        const files = e.target.files; // FileList object
+        for (let i in files) {
+            const reader = new FileReader();
+            //noinspection JSUnfilteredForInLoop
+            reader.onload = (function (theFile) {
+                reader.readAsArrayBuffer(theFile);
+                reader.addEventListener("load", function () {
+                    const arrayBuffer = reader.result;
+                    const byteArray = new Uint8Array(arrayBuffer);
+                    trees.push(TreeProto.Tree.deserializeBinary(byteArray));
+                    if (trees.length === files.length) {
+                        drawTrees(trees);
                     }
-                    for (let i = 0; i < trees.length; i++) {
-                        new Drawer(trees[i], minStartTime, maxFinishTime);
-                    }
-                }
-                // console.log(tree);
-                // console.log(TreeProto.Tree.prototype);
-                // console.log(TreeProto.Tree);
-                // const trees = jspb.Message.getRepeatedWrapperField(
-                //     TreeProto,
-                //     TreeProto.Tree);
-                // console.log(trees);
-            });
-        })(file);
+                });
+            })(files[i]);
+        }
     });
 });
