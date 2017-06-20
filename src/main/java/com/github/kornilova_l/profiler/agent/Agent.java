62c1bc21ef467956625b9c66f9217262486a140d
kornilova-l <kornilova203@gmail.com>
2017-06-20 18:24:05 +0300

    Deleted logging tread from agent

--- src/main/java/com/github/kornilova_l/profiler/agent/Agent.java
+++ src/main/java/com/github/kornilova_l/profiler/agent/Agent.java
@@ -3,18 +3,11 @@ package com.github.kornilova_l.profiler.agent;
 import java.io.IOException;
 import java.lang.instrument.Instrumentation;
 
-import com.github.kornilova_l.profiler.logger.Logger;
-import com.github.kornilova_l.profiler.logger.WaitingLoggingToFinish;
-
 public class Agent {
 
     public static void premain(String configFile, Instrumentation inst) throws IOException {
         System.out.println("I am an agent!");
-        Thread logger = new Thread(new Logger(), "logging thread");
-        logger.setDaemon(true);
-        logger.start();
 
-        Runtime.getRuntime().addShutdownHook( new WaitingLoggingToFinish("shutdown-hook"));
         inst.addTransformer(new ProfilingClassFileTransformer(configFile));
     }
 }
