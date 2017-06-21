1749e7db4571267afa58876d6f8edaf6155b5ec4
kornilova-l <kornilova203@gmail.com>
2017-06-21 18:55:37 +0300

    Implemented drawing labels

--- visualization/js/src/drawer.js
+++ visualization/js/src/drawer.js
@@ -1,5 +1,5 @@
 const MAIN_WIDTH = 700;
-const LAYER_HEIGHT = 15;
+const LAYER_HEIGHT = 19;
 const LAYER_GAP = 1;
 const COLORS = ["#18A3FA", "#0887d7"];
 
@@ -8,7 +8,7 @@ class Drawer {
         this.tree = tree;
         this.stage = null;
         this.width = this.tree.getWidth();
-        const fullDuration = maxFinishTime - minStartTime
+        const fullDuration = maxFinishTime - minStartTime;
         this.canvasWidth = this.width / fullDuration * MAIN_WIDTH;
         this.canvasHeight = (LAYER_HEIGHT + LAYER_GAP) * this.tree.getDepth() + 70;
         this.threadId = this.tree.getTreeInfo().getThreadId();
@@ -61,16 +61,26 @@ class Drawer {
     };
 
     _drawNode(node, depth, colorId) {
+        const shape = this._drawRectangle(node, depth, colorId);
+        this._drawLabel(node, depth, shape);
+    }
+
+    _drawRectangle(node, depth, colorId) {
         const shape = new createjs.Shape();
         shape.graphics
             .beginFill(COLORS[colorId])
             .drawRect(0, 0, this.canvasWidth, LAYER_HEIGHT);
-        const offsetX = (node.getOffset() / this.width) * this.canvasWidth;
+        const offsetX = this._getOffsetXForNode(node);
         const scaleX = node.getWidth() / this.width;
-        shape.setTransform(offsetX, this._flipY(depth * 16), scaleX);
+        shape.setTransform(offsetX, this._flipY(depth * (LAYER_GAP + LAYER_HEIGHT)), scaleX);
         console.log(`draw: ${depth}\t${scaleX}\t${node.getNodeInfo().getMethodName()}`);
         this._createPopup(node, shape);
         this.stage.addChild(shape);
+        return shape;
+    }
+
+    _getOffsetXForNode(node) {
+        return (node.getOffset() / this.width) * this.canvasWidth;
     }
 
     _createPopup(node, shape) {
@@ -87,7 +97,23 @@ class Drawer {
             popup.show();
         });
         shape.addEventListener("mouseout", () => {
-           popup.hide();
+            popup.hide();
         });
+    }
+
+    _drawLabel(node, depth, shape) {
+        const text = new createjs.Text(
+            node.getNodeInfo().getClassName().split("/").join(".") + "." + node.getNodeInfo().getMethodName(),
+            (LAYER_HEIGHT - 2) + "px Arial",
+            "#fff"
+        );
+        text.x = this._getOffsetXForNode(node) + 2;
+        text.y = this._flipY(depth * (LAYER_GAP + LAYER_HEIGHT));
+        const newShape = shape.clone();
+        newShape.scaleX = shape.scaleX * 0.9;
+        text.mask = newShape;
+        this.stage.setChildIndex(text, this.stage.getNumChildren() - 1);
+
+        this.stage.addChild(text);
     }
 }
