9423ff694d8b5f6c424f22fa8f02bd2cfb397c94
kornilova-l <kornilova203@gmail.com>
2017-06-22 14:48:21 +0300

    Found where to get port of builtInServer

--- src/main/java/com/github/kornilova_l/server/ShowProfilingResultsAction.java
+++ src/main/java/com/github/kornilova_l/server/ShowProfilingResultsAction.java
@@ -3,10 +3,13 @@ package com.github.kornilova_l.server;
 import com.intellij.ide.BrowserUtil;
 import com.intellij.openapi.actionSystem.AnAction;
 import com.intellij.openapi.actionSystem.AnActionEvent;
+import org.jetbrains.ide.BuiltInServerManager;
 
 public class ShowProfilingResultsAction extends AnAction {
     @Override
     public void actionPerformed(AnActionEvent event) {
-        BrowserUtil.browse("https://github.com");
+        BrowserUtil.browse("http://localhost:" +
+                BuiltInServerManager.getInstance().getPort() +
+                "/flamegraph-profiler/results");
     }
 }
