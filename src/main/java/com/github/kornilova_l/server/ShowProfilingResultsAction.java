9b9bcaf767e38ecad8edf273d42cf049c10c1871
kornilova-l <kornilova203@gmail.com>
2017-06-22 14:32:01 +0300

    Found out how to open url in browser

--- src/main/java/com/github/kornilova_l/server/ShowProfilingResultsAction.java
+++ src/main/java/com/github/kornilova_l/server/ShowProfilingResultsAction.java
@@ -1,15 +1,12 @@
 package com.github.kornilova_l.server;
 
-import com.intellij.ide.browsers.actions.BaseOpenInBrowserAction;
+import com.intellij.ide.BrowserUtil;
 import com.intellij.openapi.actionSystem.AnAction;
 import com.intellij.openapi.actionSystem.AnActionEvent;
-
-import static com.intellij.ide.browsers.actions.OpenFileInDefaultBrowserActionKt.findUsingBrowser;
 
 public class ShowProfilingResultsAction extends AnAction {
     @Override
     public void actionPerformed(AnActionEvent event) {
-        System.out.println("ShowProfilingResultsAction");
-        BaseOpenInBrowserAction.Companion.open(event, findUsingBrowser());
+        BrowserUtil.browse("https://github.com");
     }
 }
