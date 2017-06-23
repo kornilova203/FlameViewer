01078a3416d71b06191d0edbb4b2b1f56d760141
kornilova-l <kornilova203@gmail.com>
2017-06-23 20:04:17 +0300

    Reimplemented building of original tree

---
+++ src/main/java/com/github/kornilova_l/server/trees/OriginalTreesBuilder.java
@@ -0,0 +1,47 @@
+package com.github.kornilova_l.server.trees;
+
+import com.github.kornilova_l.protos.EventProtos;
+import com.github.kornilova_l.protos.TreeProtos;
+import com.github.kornilova_l.protos.TreesProtos;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.HashMap;
+
+class OriginalTreesBuilder {
+    public static TreesProtos.Trees buildOriginalTrees(File logFile) {
+        try (InputStream inputStream = new FileInputStream(logFile)) {
+            HashMap<Long, OTBuilder> trees = new HashMap<>();
+            EventProtos.Event event = EventProtos.Event.parseDelimitedFrom(inputStream);
+            long timeOfLastEvent = event.getTime(); // is used to finish calls which does not have exit events
+            while (event != null) {
+                EventProtos.Event finalEvent = event;
+                trees.computeIfAbsent(
+                        event.getThreadId(),
+                        k -> new OTBuilder(finalEvent.getTime(), finalEvent.getThreadId())
+                ).addEvent(event);
+                timeOfLastEvent = event.getTime();
+                event = EventProtos.Event.parseDelimitedFrom(inputStream);
+            }
+            return HashMapToTrees(trees, timeOfLastEvent);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    private static TreesProtos.Trees HashMapToTrees(HashMap<Long, OTBuilder> trees, long timeOfLastEvent) {
+        TreesProtos.Trees.Builder treesBuilder = TreesProtos.Trees.newBuilder();
+        for (OTBuilder oTBuilder : trees.values()) {
+            TreeProtos.Tree tree = oTBuilder.getBuiltTree(timeOfLastEvent);
+            if (tree != null) {
+                treesBuilder.addTrees(
+                        tree
+                );
+            }
+        }
+        return treesBuilder.build();
+    }
+}
