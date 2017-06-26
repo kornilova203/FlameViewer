3e0b38f2fad4c531597856378ca869f738e9d3ac
kornilova-l <kornilova203@gmail.com>
2017-06-26 17:44:24 +0300

    Drawing outgoing calls for methods

--- visualization/static/js/src/outgoing-calls.js
+++ visualization/static/js/src/outgoing-calls.js
@@ -8,9 +8,14 @@ function drawTree(tree) {
 }
 
 $(window).on("load", function () {
-    console.log(window.location.href);
     const request = new XMLHttpRequest();
-    request.open("GET", "/flamegraph-profiler/trees/outgoing-calls", true);
+    console.log(window.location.href);
+    const parameters = window.location.href.split("?")[1];
+    if (parameters === undefined) {
+        request.open("GET", "/flamegraph-profiler/trees/outgoing-calls", true);
+    } else {
+        request.open("GET", "/flamegraph-profiler/trees/outgoing-calls?" + parameters, true);
+    }
     request.responseType = "arraybuffer";
 
     request.onload = function () {
