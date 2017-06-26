41c1755745c34ee9fefe11ea1f5d6445f90bc042
kornilova-l <kornilova203@gmail.com>
2017-06-26 13:44:51 +0300

    Added base script for outgoing calls

--- visualization/static/js/src/outgoing-calls.js
+++ visualization/static/js/src/outgoing-calls.js
@@ -1,35 +1,23 @@
 /**
  * Main function
  */
-const TreesProto = require('../generated/trees_pb');
+const TreeProto = require('../generated/tree_pb');
 
-function drawTrees(trees) {
-    let minStartTime = trees[0].getTreeInfo().getStartTime();
-    let maxFinishTime = trees[0].getTreeInfo().getStartTime() + trees[0].getWidth();
-    for (let i = 1; i < trees.length; i++) {
-        const startTime = trees[i].getTreeInfo().getStartTime();
-        if (startTime < minStartTime) {
-            minStartTime = startTime;
-        }
-        if (startTime + trees[i].getWidth() > maxFinishTime) {
-            maxFinishTime = startTime + trees[i].getWidth();
-        }
-    }
-    for (let i = 0; i < trees.length; i++) {
-        new Drawer(trees[i], minStartTime, maxFinishTime);
-    }
+function drawTree(tree) {
+    new BaseDrawer(tree);
 }
 
 $(window).on("load", function () {
     const request = new XMLHttpRequest();
-    request.open("GET", "/flamegraph-profiler/trees/call-tree", true);
+    request.open("GET", "/flamegraph-profiler/trees/outgoing-calls", true);
     request.responseType = "arraybuffer";
 
     request.onload = function () {
         const arrayBuffer = request.response;
         const byteArray = new Uint8Array(arrayBuffer);
-        const trees = TreesProto.Trees.deserializeBinary(byteArray).getTreesList();
-        drawTrees(trees);
+        //noinspection JSUnresolvedVariable
+        const tree = TreeProto.Tree.deserializeBinary(byteArray);
+        drawTree(tree);
     };
     request.send();
 });
