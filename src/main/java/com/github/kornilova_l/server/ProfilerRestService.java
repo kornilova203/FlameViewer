06baebfc71ddb964c392f6053adc270701d7428c
kornilova-l <kornilova203@gmail.com>
2017-06-26 15:50:51 +0300

    Remove unused parameter

--- src/main/java/com/github/kornilova_l/server/ProfilerRestService.java
+++ src/main/java/com/github/kornilova_l/server/ProfilerRestService.java
@@ -54,29 +54,29 @@ public class ProfilerRestService extends RestService {
         switch (uri) {
             case ServerNames.CALL_TREE:
                 LOG.info("call-tree.html");
-                sendStatic(request, context, ServerNames.MAIN_NAME + "/call-tree.html", "text/html");
+                sendStatic(context, ServerNames.MAIN_NAME + "/call-tree.html", "text/html");
                 break;
             case ServerNames.OUTGOING_CALLS:
                 LOG.info("outgoing-calls.html");
-                sendStatic(request, context, ServerNames.MAIN_NAME + "/outgoing-calls.html", "text/html");
+                sendStatic(context, ServerNames.MAIN_NAME + "/outgoing-calls.html", "text/html");
                 break;
             case ServerNames.CALL_TREE_JS_REQUEST:
                 LOG.info("CALL_TREE_JS_REQUEST");
-                sendTrees(request, context, treeBuilder.getCallTree());
+                sendTrees(context, treeBuilder.getCallTree());
                 break;
             case ServerNames.OUTGOING_CALLS_JS_REQUEST:
                 LOG.info("OUTGOING_CALLS_JS_REQUEST");
-                sendTree(request, context, treeBuilder.getOutgoingCalls());
+                sendTree(context, treeBuilder.getOutgoingCalls());
                 break;
             default:
                 if (ServerNames.CSS_PATTERN.matcher(uri).matches()) {
                     LOG.info("CSS");
-                    sendStatic(request, context, uri, "text/css");
+                    sendStatic(context, uri, "text/css");
                 } else if (ServerNames.JS_PATTERN.matcher(uri).matches()) {
                     LOG.info("JS");
-                    sendStatic(request, context, uri, "text/javascript");
+                    sendStatic(context, uri, "text/javascript");
                 } else if (ServerNames.FONT_PATTERN.matcher(uri).matches()) {
-                    sendStatic(request, context, uri, "application/octet-stream");
+                    sendStatic(context, uri, "application/octet-stream");
                 } else {
                     return "Not Found";
                 }
@@ -84,32 +84,29 @@ public class ProfilerRestService extends RestService {
         return null;
     }
 
-    private static void sendTrees(FullHttpRequest request,
-                                  ChannelHandlerContext context,
+    private static void sendTrees(ChannelHandlerContext context,
                                   TreesProtos.Trees trees) {
         ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
         try {
             trees.writeTo(outputStream);
-            sendBytes(request, context, "application/octet-stream", outputStream.toByteArray());
+            sendBytes(context, "application/octet-stream", outputStream.toByteArray());
         } catch (IOException e) {
             LOG.error(e);
         }
     }
 
-    private static void sendTree(FullHttpRequest request,
-                                 ChannelHandlerContext context,
+    private static void sendTree(ChannelHandlerContext context,
                                  TreeProtos.Tree tree) {
         ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
         try {
             tree.writeTo(outputStream);
-            sendBytes(request, context, "application/octet-stream", outputStream.toByteArray());
+            sendBytes(context, "application/octet-stream", outputStream.toByteArray());
         } catch (IOException e) {
             LOG.error(e);
         }
     }
 
-    private static void sendStatic(FullHttpRequest request,
-                                   ChannelHandlerContext context,
+    private static void sendStatic(ChannelHandlerContext context,
                                    String fileName,
                                    String contentType) throws IOException {
         LOG.info("Got filename: " + fileName);
@@ -120,12 +117,11 @@ public class ProfilerRestService extends RestService {
                         new File(filePath)
                 )
         ) {
-            sendBytes(request, context, contentType, IOUtils.toByteArray(inputStream));
+            sendBytes(context, contentType, IOUtils.toByteArray(inputStream));
         }
     }
 
-    private static void sendBytes(FullHttpRequest request,
-                                  ChannelHandlerContext context,
+    private static void sendBytes(ChannelHandlerContext context,
                                   String contentType,
                                   byte[] bytes) {
         FullHttpResponse response = new DefaultFullHttpResponse(
