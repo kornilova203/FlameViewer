ccd6dbbafe19dae33ba44c88efc48ac3f98744c8
kornilova-l <kornilova203@gmail.com>
2017-06-29 19:40:51 +0300

    Implemented Program Runner which extends DefaultJavaProgramRunner

--- src/main/java/com/github/kornilova_l/plugin/ProfilerProgramRunner.java
+++ src/main/java/com/github/kornilova_l/plugin/ProfilerProgramRunner.java
@@ -1,19 +1,18 @@
 package com.github.kornilova_l.plugin;
 
 import com.intellij.execution.ExecutionException;
-import com.intellij.execution.configurations.JavaParameters;
-import com.intellij.execution.configurations.RunProfile;
-import com.intellij.execution.configurations.RunnerSettings;
-import com.intellij.execution.runners.JavaPatchableProgramRunner;
+import com.intellij.execution.configurations.*;
+import com.intellij.execution.impl.DefaultJavaProgramRunner;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.Objects;
 
 
-public class ProfilerProgramRunner extends JavaPatchableProgramRunner {
+public class ProfilerProgramRunner extends DefaultJavaProgramRunner {
     private final String RUNNER_ID = "ProfileRunnerID";
 
     public ProfilerProgramRunner() {
+        super();
         System.out.println("Patcher created");
     }
 
@@ -35,18 +34,4 @@ public class ProfilerProgramRunner extends JavaPatchableProgramRunner {
     public boolean canRun(@NotNull String executorId, @NotNull RunProfile profile) {
         return Objects.equals(executorId, ProfilerExecutor.EXECUTOR_ID);
     }
-
-    /*
-    @Override
-    public <T extends RunConfigurationBase> void updateJavaParameters(T configuration,
-                                                                      JavaParameters params,
-                                                                      RunnerSettings runnerSettings) throws ExecutionException {
-        ConfigStorage.State state =
-                (ConfigStorage.State) configuration.getProject().getComponent(PersistentStateComponent.class).getState();
-        params.getVMParametersList().add(
-                "-javaagent:/home/lk/java-profiling-plugin/build/libs/javaagent.jar=" +
-                        PathManager.getSystemPath()
-        );
-    }
-     */
 }
