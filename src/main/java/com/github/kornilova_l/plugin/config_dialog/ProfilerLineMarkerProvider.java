77b9758bf5fc404ff88b07e0d8e3bb690f199b44
kornilova-l <kornilova203@gmail.com>
2017-07-04 19:36:41 +0300

    Implemented simple LineMarkerProvider

---
+++ src/main/java/com/github/kornilova_l/plugin/config_dialog/ProfilerLineMarkerProvider.java
@@ -0,0 +1,29 @@
+package com.github.kornilova_l.plugin.config_dialog;
+
+import com.intellij.codeInsight.daemon.RelatedItemLineMarkerInfo;
+import com.intellij.codeInsight.daemon.RelatedItemLineMarkerProvider;
+import com.intellij.codeInsight.navigation.NavigationGutterIconBuilder;
+import com.intellij.icons.AllIcons;
+import com.intellij.psi.PsiElement;
+import com.intellij.psi.PsiLiteralExpression;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.Collection;
+
+public class ProfilerLineMarkerProvider extends RelatedItemLineMarkerProvider {
+    @Override
+    protected void collectNavigationMarkers(@NotNull PsiElement element,
+                                            Collection<? super RelatedItemLineMarkerInfo> result) {
+        if (element instanceof PsiLiteralExpression) {
+            PsiLiteralExpression literalExpression = (PsiLiteralExpression) element;
+            String value = literalExpression.getValue() instanceof String ? (String) literalExpression.getValue() : null;
+            if (value != null && value.startsWith("simple" + ":")) {
+                NavigationGutterIconBuilder<PsiElement> builder =
+                        NavigationGutterIconBuilder.create(AllIcons.Nodes.Package)
+                                .setTargets(element)
+                                .setTooltipText("Navigate to a simple property");
+                result.add(builder.createLineMarkerInfo(element));
+            }
+        }
+    }
+}
