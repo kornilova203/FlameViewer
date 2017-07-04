c262950f473f437b1fa52c33547dd939ba8d0143
kornilova-l <kornilova203@gmail.com>
2017-07-04 19:35:49 +0300

    Added reference field to ConfigNode

--- src/main/java/com/github/kornilova_l/plugin/config/ConfigNode.java
+++ src/main/java/com/github/kornilova_l/plugin/config/ConfigNode.java
@@ -1,5 +1,7 @@
 package com.github.kornilova_l.plugin.config;
 
+import com.intellij.psi.PsiJavaReference;
+
 import java.util.Collection;
 
 public class ConfigNode {
@@ -13,6 +15,7 @@ public class ConfigNode {
     public Type type;
     public Collection<ConfigNode> children; // not for method type
     public Collection<Object> parameters; // for method type
+    private PsiJavaReference reference;
 
     public ConfigNode() {
     }
