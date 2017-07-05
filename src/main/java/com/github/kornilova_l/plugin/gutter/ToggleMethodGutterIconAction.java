43ebf585a8d95419258b390d4610384f40b7175d
kornilova-l <kornilova203@gmail.com>
2017-07-05 13:06:04 +0300

    Refactored ToggleMethodGutterIconAction

--- src/main/java/com/github/kornilova_l/plugin/gutter/ToggleMethodGutterIconAction.java
+++ src/main/java/com/github/kornilova_l/plugin/gutter/ToggleMethodGutterIconAction.java
@@ -25,22 +25,30 @@ import com.intellij.xdebugger.ui.DebuggerColors;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
-public class ToggleMethodGutterIcon extends AnAction {
+public class ToggleMethodGutterIconAction extends AnAction {
     @Override
-    public void actionPerformed(AnActionEvent e) {
-        PsiElementAndDocument method = getMethodAndDocument(e);
+    public void actionPerformed(AnActionEvent event) {
+        final Project project = event.getData(CommonDataKeys.PROJECT);
+        if (project == null) {
+            return;
+        }
+        final Editor editor = getEditor(event, project);
+        if (editor == null) {
+            return;
+        }
+        PsiMethod method = getMethod(event, editor, project);
         if (method == null) {
             return;
         }
-        assert e.getProject() != null;
-        setIcon(e.getProject(), method);
+        assert event.getProject() != null;
+        setIcon(method, project, editor.getDocument());
     }
 
-    private static void setIcon(@NotNull Project project, @NotNull PsiElementAndDocument methodAndDoc) {
-        MarkupModelEx markupModel = (MarkupModelEx) DocumentMarkupModel.forDocument(methodAndDoc.getDocument(), project, true);
+    private static void setIcon(PsiMethod method, Project project, Document document) {
+        MarkupModelEx markupModel = (MarkupModelEx) DocumentMarkupModel.forDocument(document, project, true);
         RangeHighlighter highlighter = markupModel.addRangeHighlighter(
-                methodAndDoc.getPsiElement().getTextOffset(),
-                methodAndDoc.getPsiElement().getTextOffset() + 1,
+                method.getTextOffset(),
+                method.getTextOffset() + 1,
                 DebuggerColors.BREAKPOINT_HIGHLIGHTER_LAYER,
                 null,
                 HighlighterTargetArea.EXACT_RANGE);
@@ -48,58 +56,52 @@ public class ToggleMethodGutterIcon extends AnAction {
     }
 
     @Nullable
-    private static PsiElementAndDocument getMethodAndDocument(@NotNull AnActionEvent event) {
-        final Project project = event.getData(CommonDataKeys.PROJECT);
-        if(project == null) {
-            return null;
+    private static Editor getEditor(@NotNull AnActionEvent event, @NotNull Project project) {
+        Editor editor = event.getData(CommonDataKeys.EDITOR);
+        if (editor == null) {
+            return FileEditorManager.getInstance(project).getSelectedTextEditor();
         }
+        return editor;
+    }
 
-        PsiElement method = null;
-        Document document = null;
+    @Nullable
+    private static PsiMethod getMethod(@NotNull AnActionEvent event,
+                                       @NotNull Editor editor,
+                                       @NotNull Project project) {
+        PsiMethod method = null;
 
         if (ActionPlaces.PROJECT_VIEW_POPUP.equals(event.getPlace()) ||
                 ActionPlaces.STRUCTURE_VIEW_POPUP.equals(event.getPlace()) ||
                 ActionPlaces.FAVORITES_VIEW_POPUP.equals(event.getPlace()) ||
                 ActionPlaces.NAVIGATION_BAR_POPUP.equals(event.getPlace())) {
             final PsiElement psiElement = event.getData(CommonDataKeys.PSI_ELEMENT);
-            if(psiElement instanceof PsiMethod) {
+            if (psiElement instanceof PsiMethod) {
                 final PsiFile containingFile = psiElement.getContainingFile();
                 if (containingFile != null) {
-                    method = psiElement;
-                    document = PsiDocumentManager.getInstance(project).getDocument(containingFile);
+                    method = (PsiMethod) psiElement;
+                }
+            }
+        } else {
+            Document document = editor.getDocument();
+            PsiFile file = PsiDocumentManager.getInstance(project).getPsiFile(document);
+            if (file != null) {
+                final VirtualFile virtualFile = file.getVirtualFile();
+                FileType fileType = virtualFile != null ? virtualFile.getFileType() : null;
+                if (StdFileTypes.JAVA == fileType || StdFileTypes.CLASS == fileType) {
+                    method = findMethod(project, editor);
                 }
             }
         }
-        else {
-            Editor editor = event.getData(CommonDataKeys.EDITOR);
-            if(editor == null) {
-                editor = FileEditorManager.getInstance(project).getSelectedTextEditor();
-            }
-            if (editor != null) {
-                document = editor.getDocument();
-                PsiFile file = PsiDocumentManager.getInstance(project).getPsiFile(document);
-                if (file != null) {
-                    final VirtualFile virtualFile = file.getVirtualFile();
-                    FileType fileType = virtualFile != null ? virtualFile.getFileType() : null;
-                    if (StdFileTypes.JAVA == fileType || StdFileTypes.CLASS  == fileType) {
-                        method = findMethod(project, editor);
-                    }
-                }
-            }
-        }
-        if (method == null || document == null) {
-            return null;
-        }
-        return new PsiElementAndDocument(method, document);
+        return method;
     }
 
     @Nullable
-    private static PsiElement findMethod(Project project, Editor editor) {
+    private static PsiMethod findMethod(Project project, Editor editor) {
         if (editor == null) {
             return null;
         }
         PsiFile psiFile = PsiDocumentManager.getInstance(project).getPsiFile(editor.getDocument());
-        if(psiFile == null) {
+        if (psiFile == null) {
             return null;
         }
         final int offset = CharArrayUtil.shiftForward(editor.getDocument().getCharsSequence(), editor.getCaretModel().getOffset(), " \t");
