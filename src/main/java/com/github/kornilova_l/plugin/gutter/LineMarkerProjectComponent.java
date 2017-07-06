cfbf5d433711205102b44805a69e495370aeba11
kornilova-l <kornilova203@gmail.com>
2017-07-06 11:01:29 +0300

    Added PsiDocumentManager.Listener

--- src/main/java/com/github/kornilova_l/plugin/gutter/LineMarkerProjectComponent.java
+++ src/main/java/com/github/kornilova_l/plugin/gutter/LineMarkerProjectComponent.java
@@ -1,11 +1,14 @@
 package com.github.kornilova_l.plugin.gutter;
 
+import com.github.kornilova_l.plugin.ProjectConfigManager;
+import com.github.kornilova_l.plugin.config.ConfigStorage;
 import com.intellij.openapi.components.AbstractProjectComponent;
 import com.intellij.openapi.editor.Document;
 import com.intellij.openapi.project.Project;
-import com.intellij.psi.PsiDocumentManager;
-import com.intellij.psi.PsiFile;
+import com.intellij.psi.*;
 import org.jetbrains.annotations.NotNull;
+
+import java.util.Objects;
 
 public class LineMarkerProjectComponent extends AbstractProjectComponent {
     protected LineMarkerProjectComponent(Project project) {
@@ -14,17 +17,51 @@ public class LineMarkerProjectComponent extends AbstractProjectComponent {
 
     @Override
     public void projectOpened() {
-        System.out.println("ya rodilsya");
+        ConfigStorage.Config config = ProjectConfigManager.getConfig(myProject);
+        LineMarkersHolder lineMarkersHolder = myProject.getComponent(LineMarkersHolder.class);
         PsiDocumentManager.getInstance(myProject)
                 .addListener(new PsiDocumentManager.Listener() {
                     @Override
                     public void documentCreated(@NotNull final Document document, PsiFile psiFile) {
-                        System.out.println("doc created");
+                        if (psiFile == null) {
+                            return;
+                        }
+                        System.out.println(psiFile.getName());
+                        if (Objects.equals(psiFile.getFileType().getDefaultExtension(), "java")) {
+                            for (PsiElement child : psiFile.getChildren()) {
+                                if (child instanceof PsiClass) {
+                                    processMethods(((PsiClass) child), document);
+                                }
+                            }
+                        }
                     }
 
                     @Override
-                    public void fileCreated(@NotNull PsiFile file, @NotNull Document document) {
-                        System.out.println("file created");
+                    public void fileCreated(@NotNull PsiFile psiFile, @NotNull Document document) {
+                        System.out.println(psiFile.getName());
+                        if (Objects.equals(psiFile.getFileType().getDefaultExtension(), "java")) {
+                            for (PsiElement child : psiFile.getChildren()) {
+                                if (child instanceof PsiClass) {
+                                    processMethods(((PsiClass) child), document);
+                                }
+                            }
+                        }
+                    }
+
+                    private void processMethods(PsiClass psiClass, Document document) {
+                        System.out.println("Class: " + psiClass.getName());
+                        for (PsiElement maybeMethod : psiClass.getChildren()) {
+                            if (maybeMethod instanceof PsiMethod) {
+                                System.out.println("Method: " + ((PsiMethod) maybeMethod).getName());
+                                if (config.contains(((PsiMethod) maybeMethod))) {
+                                    lineMarkersHolder.setIcon(
+                                            (PsiMethod) maybeMethod,
+                                            myProject,
+                                            document
+                                    );
+                                }
+                            }
+                        }
                     }
                 });
     }
---
+++ src/main/java/com/github/kornilova_l/plugin/gutter/LineMarkersHolder.java
@@ -0,0 +1,41 @@
+package com.github.kornilova_l.plugin.gutter;
+
+import com.intellij.openapi.components.AbstractProjectComponent;
+import com.intellij.openapi.editor.Document;
+import com.intellij.openapi.editor.ex.MarkupModelEx;
+import com.intellij.openapi.editor.impl.DocumentMarkupModel;
+import com.intellij.openapi.editor.markup.HighlighterTargetArea;
+import com.intellij.openapi.editor.markup.RangeHighlighter;
+import com.intellij.openapi.project.Project;
+import com.intellij.psi.PsiMethod;
+import com.intellij.xdebugger.ui.DebuggerColors;
+
+import java.util.HashMap;
+
+import static com.github.kornilova_l.plugin.config.ConfigStorage.Config.getQualifiedName;
+
+public class LineMarkersHolder extends AbstractProjectComponent {
+    private final HashMap<String, RangeHighlighter> rangeHighlighters = new HashMap<>();
+
+    protected LineMarkersHolder(Project project) {
+        super(project);
+    }
+
+    public void setIcon(PsiMethod method, Project project, Document document) {
+        MarkupModelEx markupModel = (MarkupModelEx) DocumentMarkupModel.forDocument(document, project, true);
+        RangeHighlighter highlighter = markupModel.addRangeHighlighter(
+                method.getTextOffset(),
+                method.getTextOffset() + 1,
+                DebuggerColors.BREAKPOINT_HIGHLIGHTER_LAYER,
+                null,
+                HighlighterTargetArea.EXACT_RANGE);
+        highlighter.setGutterIconRenderer(new ProfilerGutterIconRenderer());
+        rangeHighlighters.put(getQualifiedName(method), highlighter);
+    }
+
+    public void removeIcon(PsiMethod method, Project project, Document document) {
+            RangeHighlighter highlighter = rangeHighlighters.get(getQualifiedName(method));
+            MarkupModelEx markupModel = (MarkupModelEx) DocumentMarkupModel.forDocument(document, project, true);
+            markupModel.removeHighlighter(highlighter);
+    }
+}
