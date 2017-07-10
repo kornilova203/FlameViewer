a0fa2b44739c8badce16d13b13ef7999af3d46b3
kornilova-l <kornilova203@gmail.com>
2017-07-10 17:38:05 +0300

    Added form for uploading file and POST request

--- src/main/java/com/github/kornilova_l/server/ProfilerHttpRequestHandler.java
+++ src/main/java/com/github/kornilova_l/server/ProfilerHttpRequestHandler.java
@@ -6,6 +6,7 @@ import com.github.kornilova_l.protos.TreesProtos;
 import com.github.kornilova_l.server.trees.TreeManager;
 import com.google.gson.Gson;
 import io.netty.buffer.Unpooled;
+import io.netty.channel.Channel;
 import io.netty.channel.ChannelFuture;
 import io.netty.channel.ChannelFutureListener;
 import io.netty.channel.ChannelHandlerContext;
@@ -13,8 +14,10 @@ import io.netty.handler.codec.http.*;
 import org.apache.commons.compress.utils.IOUtils;
 import org.jetbrains.annotations.Nullable;
 import org.jetbrains.ide.HttpRequestHandler;
+import org.jetbrains.io.Responses;
 
 import java.io.*;
+import java.util.Objects;
 
 public class ProfilerHttpRequestHandler extends HttpRequestHandler {
 
@@ -105,12 +108,53 @@ public class ProfilerHttpRequestHandler extends HttpRequestHandler {
         f.addListener(ChannelFutureListener.CLOSE);
     }
 
+    private static boolean processPostMethod(QueryStringDecoder urlDecoder, FullHttpRequest fullHttpRequest, ChannelHandlerContext context) {
+        String uri = urlDecoder.path(); // without get parameters
+        if (Objects.equals(uri, ServerNames.UPLOAD_FILE)) {
+            LOG.info("Got file: " + fullHttpRequest.headers().get("File-Name"));
+            if (supportedExtension(fullHttpRequest.headers().get("File-Name"))) {
+                sendStatus(HttpResponseStatus.OK, context.channel());
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private static void sendStatus(HttpResponseStatus status, Channel channel) {
+        DefaultFullHttpResponse response = new DefaultFullHttpResponse(HttpVersion.HTTP_1_1, status);
+        HttpUtil.setContentLength(response, 0);
+        Responses.addCommonHeaders(response);
+        Responses.addNoCache(response);
+        response.headers().set("X-Frame-Options", "Deny");
+        Responses.send(response, channel, true);
+    }
+
+    private static boolean supportedExtension(String fileName) {
+        String extension = fileName.substring(fileName.indexOf(".") + 1, fileName.length());
+        return Objects.equals(extension, "jfr") ||
+                Objects.equals(extension, "ser");
+    }
+
     @Override
     public boolean process(QueryStringDecoder urlDecoder,
                            FullHttpRequest fullHttpRequest,
-                           ChannelHandlerContext context) throws IOException {
+                           ChannelHandlerContext context) {
+        LOG.info(fullHttpRequest.method() + " Request: " + urlDecoder.uri());
+        if (fullHttpRequest.method() == HttpMethod.POST) {
+            return processPostMethod(urlDecoder, fullHttpRequest, context);
+        } else {
+            return processGetMethod(urlDecoder, context);
+        }
+    }
+
+    @Override
+    public boolean isSupported(FullHttpRequest request) {
+        return request.method() == HttpMethod.POST ||
+                request.method() == HttpMethod.GET;
+    }
+
+    private boolean processGetMethod(QueryStringDecoder urlDecoder, ChannelHandlerContext context) {
         String uri = urlDecoder.path(); // without get parameters
-        LOG.info("Request: " + uri);
         switch (uri) {
             case ServerNames.CALL_TREE:
                 LOG.info("call-tree.html");
@@ -126,7 +170,11 @@ public class ProfilerHttpRequestHandler extends HttpRequestHandler {
                 break;
             case ServerNames.SELECT_FILE:
                 LOG.info("select-file.html");
-                sendStatic(context, ServerNames.MAIN_NAME + "/select-file.html", "text/html");
+                try {
+                    sendStatic(context, ServerNames.MAIN_NAME + "/select-file.html", "text/html");
+                } catch (IOException e) {
+                    return false;
+                }
                 break;
             case ServerNames.FILE_LIST:
                 LOG.info("file list");
@@ -178,17 +226,21 @@ public class ProfilerHttpRequestHandler extends HttpRequestHandler {
                 }
                 break;
             default:
-                if (ServerNames.CSS_PATTERN.matcher(uri).matches()) {
-                    LOG.info("CSS");
-                    sendStatic(context, uri, "text/css");
-                } else if (ServerNames.JS_PATTERN.matcher(uri).matches()) {
-                    LOG.info("JS");
-                    sendStatic(context, uri, "text/javascript");
-                } else if (ServerNames.FONT_PATTERN.matcher(uri).matches()) {
-                    sendStatic(context, uri, "application/octet-stream");
-                } else if (ServerNames.PNG_PATTERN.matcher(uri).matches()) {
-                    sendStatic(context, uri, "image/png");
-                } else {
+                try {
+                    if (ServerNames.CSS_PATTERN.matcher(uri).matches()) {
+                        LOG.info("CSS");
+                        sendStatic(context, uri, "text/css");
+                    } else if (ServerNames.JS_PATTERN.matcher(uri).matches()) {
+                        LOG.info("JS");
+                        sendStatic(context, uri, "text/javascript");
+                    } else if (ServerNames.FONT_PATTERN.matcher(uri).matches()) {
+                        sendStatic(context, uri, "application/octet-stream");
+                    } else if (ServerNames.PNG_PATTERN.matcher(uri).matches()) {
+                        sendStatic(context, uri, "image/png");
+                    } else {
+                        return false;
+                    }
+                } catch (IOException e) {
                     return false;
                 }
         }
--- visualization/static/js/src/select-file.js
+++ visualization/static/js/src/select-file.js
@@ -1,4 +1,49 @@
 $(window).on("load", function () {
+    getFilesList();
+    listenInput();
+});
+
+function appendWrongExtension() {
+    if ($(".extension-error").length === 0) {
+        $(".file-form").append("<p class='extension-error'>Wrong file extension</p>");
+    }
+}
+
+function removeWrongExtension() {
+    $(".extension-error").remove();
+}
+
+function sendFile(file) {
+    const request = new XMLHttpRequest();
+    request.onreadystatechange = (e) => {
+        switch (e.currentTarget.status) {
+            case 404:
+                appendWrongExtension();
+                break;
+            case 200:
+                removeWrongExtension();
+                break;
+        }
+    };
+    request.open("POST", "/flamegraph-profiler/upload-file", true);
+    request.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+    request.setRequestHeader('File-Name', file.name);
+    request.send(file);
+
+}
+
+function listenInput() {
+    $('#file').on('change', function (e) {
+        const file = e.target.files[0]; // FileList object
+        const reader = new FileReader();
+
+        reader.onload = (function (theFile) {
+            sendFile(theFile);
+        })(file);
+    });
+}
+
+function getFilesList() {
     const request = new XMLHttpRequest();
     request.open("GET", "/flamegraph-profiler/file-list", true);
     request.responseType = "json";
@@ -15,4 +60,4 @@ $(window).on("load", function () {
         }
     };
     request.send();
-});
+}
