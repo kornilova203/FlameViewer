67d755338f0c6d66f07de22de8d0cb447fa765a0
kornilova-l <kornilova203@gmail.com>
2017-07-14 16:11:08 +0300

    Do not open dialog if project is not open

--- src/main/java/com/github/kornilova_l/flamegraph/plugin/ui/config_dialog/ShowProfilerDialogAction.java
+++ src/main/java/com/github/kornilova_l/flamegraph/plugin/ui/config_dialog/ShowProfilerDialogAction.java
@@ -6,7 +6,9 @@ import com.intellij.openapi.actionSystem.AnActionEvent;
 public class ShowProfilerDialogAction extends AnAction {
     @Override
     public void actionPerformed(AnActionEvent e) {
-        assert (e.getProject() != null);
+        if (e.getProject() == null) {
+            return;
+        }
         new ChangeConfigurationDialog(e.getProject()).show();
     }
 }
