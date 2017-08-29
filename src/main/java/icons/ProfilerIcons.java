af20c982c3aec7680cbc3d3e1efc48407264eb75
kornilova <kornilova203@gmail.com>
2017-08-29 21:19:38 +0300

    Added new icons

--- src/main/java/icons/ProfilerIcons.java
+++ src/main/java/icons/ProfilerIcons.java
@@ -5,6 +5,6 @@ import com.intellij.openapi.util.IconLoader;
 import javax.swing.*;
 
 public class ProfilerIcons {
-    public static final Icon runIcon = IconLoader.getIcon("/icons/flame-run16.png", ProfilerIcons.class);
-    public static final Icon flameIcon = IconLoader.getIcon("/icons/flame16.png", ProfilerIcons.class);
+    public static final Icon runIcon = IconLoader.getIcon("/icons/flame-run.png", ProfilerIcons.class);
+    public static final Icon flameIcon = IconLoader.getIcon("/icons/flame.png", ProfilerIcons.class);
 }
