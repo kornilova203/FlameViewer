5deb7725bdd56af7bef6ffff8303062188da3e72
kornilova-l <kornilova203@gmail.com>
2018-07-11 11:19:31 +0300

    Automatically generate Closure Templates and browserify js files
    Fix task dependencies

--- visualization/static/js/src/deserializer.js
+++ visualization/static/js/src/deserializer.js
@@ -1,6 +1,6 @@
-const TreeProto = require('../generated/tree');
-const TreesProto = require('../generated/trees');
-const TreesPreviewProtos = require('../generated/treespreview');
+const TreeProto = require('../generated/com/github/kornilova_l/flamegraph/proto/tree_pb');
+const TreesProto = require('../generated/com/github/kornilova_l/flamegraph/proto/trees_pb');
+const TreesPreviewProtos = require('../generated/com/github/kornilova_l/flamegraph/proto/trees_preview_pb');
 
 
 module.exports.deserializeTree = function (byteArray) {
