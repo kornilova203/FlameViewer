6035f6497f639d126359c75a7ae0f5013619a97e
kornilova-l <kornilova203@gmail.com>
2018-07-16 03:56:59 +0800

    Add cflamegraph schema

--- src/main/java/com/github/kornilova_l/flamegraph/plugin/server/converters/file_to_call_traces/cflamegraph/CompressedFlamegraphToCallTracesConverter.kt
+++ src/main/java/com/github/kornilova_l/flamegraph/plugin/server/converters/file_to_call_traces/cflamegraph/CompressedFlamegraphToCallTracesConverter.kt
@@ -7,26 +7,13 @@ import java.io.File
 
 
 /**
- * Original flamegraph format consumes a lot of memory on disk and it takes more time to parse
- * because it has lots of duplicate parts of call traces.
+ * Original flamegraph format consumes a lot of memory on disk and it takes more
+ * time to parse because it has lots of duplicate parts of call traces.
  * cflamegraph (compressed flamegraph) solves this problem.
+ * It uses FlatBuffers for data serialization and does not duplicate
+ * class names, method names and descriptions.
  *
- * Header of file contains ids of class names, method names and descriptions.
- * For example:
- * C
- * ClassName1 0
- * ClassName2 1
- * M
- * method1 0
- * method2 1
- * D
- * () 0
- * (int)int 1
- *
- * Each line of cflamegraph contains information in following format:
- * C=<class name id> M=<method name id> D=<description id> w=<width> d=<depth>
- *
- * C and D are optional.
+ * See scheme in /src/main/flatbuffers/cflamegraph_schema.fbs
  *
  * Example:
  * ._ _
@@ -34,37 +21,34 @@ import java.io.File
  * |b()____|e|_______ _
  * |Class.a__________|f|
  *
- * --C-- 1
- * Class 0
- * --M-- 6
- * a 0
- * b 1
- * c 2
- * d 3
- * e 4
- * f 5
- * --D-- 1
- * () 0
- * C0M0w100d1
- * M1w40d2
- * M2w5d3
- * M3w5d3
- * M4w5d2
- * M5w5d1
- *
- * As you can see order of lines matters
- * because if a call has bigger depth than previous it means that
- * the call is a child of previous call.
- *
  * In original flamegraph format this example would look like this:
- * a;b;c 5
- * a;b;d 5
- * a;b 10
+ * a;b();c 5
+ * a;b();d 5
+ * a;b() 10
  * a;e 5
  * a 50
  * f 5
  *
- * In cflamegraph files strings are not duplicated and equal parts of stacktraces are also not duplicated.
+ * Serialized with flatbuffers tree will have following structure:
+ * Names: {
+ *   classNames: ["Class"],
+ *   methodNames: ["a", "b", "c", "d", "e", "f"],
+ *   descriptions: ["()"]
+ * }
+ * Tree: {
+ *   nodes: [
+ *     { class_name_id: 0, method_name_id: 0, width: 100, depth: 1 },
+ *     { method_name_id: 1, description_id: 0, width: 40, depth: 2 },
+ *     { method_name_id: 2, width: 5, depth: 3 },
+ *     { method_name_id: 3, width: 5, depth: 3 },
+ *     { method_name_id: 4, width: 5, depth: 2 },
+ *     { method_name_id: 5, width: 5, depth: 1 }
+ *   ]
+ * }
+ *
+ * Order of nodes matters.
+ * If a call has bigger depth than previous it means that the call is a child
+ * of previous call.
  *
  * Converter merges unmerged stacktraces.
  */
